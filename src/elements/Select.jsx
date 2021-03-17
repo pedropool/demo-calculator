7ca6a8ceaf8c97cadc646de7e8054129cc4b0249
Pedro Pool <pedro.pool@raweng.com>
2021-03-16 23:11:19 -0600

    added placeholder to selects

--- src/elements/Select.jsx
+++ src/elements/Select.jsx
@@ -1,13 +1,19 @@
+// import react
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Select = ({name, label, options, value, onChange}) => {
+const Select = ({name = "", label = "", options, value, onChange, placeholder = false}) => {
   return (
     <div className="field">
       <label className="label">{ label }</label>
       <div className="control">
         <div className="select">
           <select name={name} value={value} onChange={onChange}>
+            {
+              placeholder && (
+                <option key={placeholder} >{placeholder}</option>
+              )
+            }
             {
               options.map(o => (
                 <option key={o.key} value={o.value} >{o.text}</option>
@@ -21,11 +27,12 @@ const Select = ({name, label, options, value, onChange}) => {
 };
 
 Select.propTypes = {
-  label: PropTypes.string.isRequired,
+  label: PropTypes.string,
   options: PropTypes.array.isRequired,
-  value: PropTypes.string,
+  value: PropTypes.string.isRequired,
   name: PropTypes.string,
-  onChange: PropTypes.func
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string
 }
 
 export default Select;
